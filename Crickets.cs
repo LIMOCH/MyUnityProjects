60917369e35829ce7bdf0dfcd56033300130b419
LIMOCH <102480515+LIMOCH@users.noreply.github.com>
2023-03-10 20:46:07 +0600

    Classwork
    the simple sctipts
    Destroy - delete a game object
    Crickets - spam "crickets" in console
    CreatePrimitives - create a steve from Minecraft

---
+++ Crickets.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    void Update()
+    {
+        print("crickets"); 
+    }
+}
