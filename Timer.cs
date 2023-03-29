53d9cf1e75184a515eff2c943d94e5a59eaa2d59
LIMOCH <102480515+LIMOCH@users.noreply.github.com>
2023-03-29 21:24:38 +0600

    FIX

--- Timer.cs
+++ Timer.cs
@@ -1,13 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
-    public float sec = 2.0f;
+    public float sec = 60.0f;
+    public int min = 1;
     void Update()
     {
-        sec += Time.deltaTime;
-        print(sec);
+        sec -= Time.deltaTime;
+        if (sec <= 0.0f)
+        {
+            if(min > 0)
+            {
+                min -= 1;
+                sec += 60;
+            }
+        }
+        print(min + " " + sec);
+        if(min == 0 && sec == 0)
+        {
+            EditorSceneManager.LoadScene("SampleScene");
+        }
     }
 }
